eb5671417d32885f283b82848150d25dfecf3180
NilsW <nils.wilmet@otipass.com>
2021-03-03 19:19:32 +0100

    WIP - Bot !register command to add user to DB and create his account

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -15,27 +15,60 @@ client.once('disconnect', () => {
     console.log('Disconnect!');
 });
 
-function callService(serv, args = null, callback = null) {
-    context.call(serv, args).then(res => {
+async function callService(serv, args = null, callback = null) {
+    await context.call(serv, args).then(res => {
         return callback ? callback(res) : res
     })
 }
+
+async function registerUser(message) {
+    const member = await message.member
+    const user = await message.author
+    const dmChannel = await member.createDM()
+    callService("user.existsByDiscordId", {
+        discord_id: member.id
+    }, async (res) => {
+        console.log("Does "+member.displayName+" exists in db ?", res)
+        if(!res.data.exists) {
+            await dmChannel.send("Hey ! Merci de vouloir t'inscrire au serveur ! Je t'ajoute et te prépare un mot de passe tout frais (pas de panique, tu pourras le modifier plus tard).\nJuste une minute...").catch(error => {
+                if(error.code == 50007) {
+                    message.channel.send("Oups, on dirait que tu ne peux pas encore recevoir mes MP, revois tes paramètres de confidentialité et retente à nouveau !")
+                    return
+                } 
+            })
+            dmChannel.startTyping()
+            callService("user.create", {
+                login: member.displayName,
+                tag: user.tag,
+                email: null,
+                discord_id: member.id,
+                bio: null
+            }, (res) => {
+                dmChannel.stopTyping()
+                dmChannel.send("Bien inscrit !")
+            })
+        } else {
+            message.channel.send(`Tu existes déjà dans notre base de données, ${member}`)
+        }
+    })
+}
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd',async member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'coucou');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    //PM to the user !
+    // Send the message, mentioning the member
+});
 
 client.on('message', async message => {
     if (message.author.bot || !message.content.startsWith(pre)) return
     //console.log("Chat command received !")
     //const serverQueue = queue.get(message.guild.id);
 
-    if (message.content.startsWith(`${pre}play`)) {
-        //execute(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${pre}skip`)) {
-        //skip(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${pre}stop`)) {
-        //stop(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${pre}math`)) {
+    if (message.content.startsWith(`${pre}math`)) {
         const msg = message.content
         let a,b = null
         if(msg.includes("+")) {
@@ -62,8 +95,20 @@ client.on('message', async message => {
         } else {
             message.channel.send("Invalid operation");
         }
+    } else if (message.content.startsWith(`${pre}register`)) {
+        await registerUser(message)
+    } else if (message.content.startsWith(`${pre}clear`)) {
+        //TODO PERMISSION CHECK
+        // Clear all messages
+        message.channel.bulkDelete(100)
+            .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+            .catch(error => {
+                console.error(error)
+                message.react("❌")
+            });
     } else {
-        message.channel.send("You need to enter a valid command!");
+        //const questionMarkEmoji = client.emojis.find(emoji => emoji.name === "question");
+        message.react("❓")
     }
 })
 const service = {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -19,6 +19,8 @@ const service = {
                 primaryKey: true
             },
             login: Sequelize.STRING,
+            tag: Sequelize.STRING,
+            email: Sequelize.STRING,
             discord_id: Sequelize.STRING,
             bio: Sequelize.TEXT
         },
@@ -34,6 +36,28 @@ const service = {
                 bio: "COUCOU C MOI"
             }).then(res => {
                 return res 
+            })
+        },
+        async existsByDiscordId(ctx) {
+            return await ctx.call("user.find", {
+                query: {
+                    discord_id: ctx.params.discord_id ? ctx.params.discord_id : -1 
+                }
+            }).then((res) => {
+                console.log(res)
+                if(res.length >= 1 && res[0].discord_id == ctx.params.discord_id) {
+                    return {
+                        data: {
+                            exists: true
+                        }
+                    }
+                } else {
+                    return {
+                        data: {
+                            exists: false
+                        }
+                    }
+                }
             })
         }
     }
